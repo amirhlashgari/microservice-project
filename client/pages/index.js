81532b9c33bd9a01e6d259d2203a8f203b2833d4
amirhossein.lashkari <amirhosseinlashgari.al@gmail.com>
2024-02-11 02:37:35 +0330

    Update Client: managing authorization, very important on how getInitialProps work

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,42 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+
+    /**
+     * VERY IMPORTANT:
+     * ----------------------
+     * requests should be made to http://{{SERVICE NAME}}.{{NAMESPACE}}.svc.cluster.local
+     * finding NAMESPACE ---> kubectl get namespace
+     * finding SERVICE NAME ---> kubectl get services -n {{NAMESPACE}} (shows services inside specific namespace, without this it will showing default namespace)
+     * add header "Host" to request, because in Ingress config we specified "host: microservice.dev", so it will be listening to this domain name only
+     */
+
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'microservice.dev',
+        },
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
